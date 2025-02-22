50760fd5f735071117feb4625c047752420d56c6
Luan <almeidaluan58@gmail.com>
2025-02-22 10:10:46 -0300

    Xriando o loop principal Conectando a classe menu e a classe game

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-import pygame
-
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-
-while True:
-    menu = Menu(self.window)
-    menu.run()
-    pass
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  # Close window
-            quit()  # end pygame
-
-
-
-
-from code.Game import Game
+from code.game import Game
 
 game = Game()
 game.run()
+
+
+
+
